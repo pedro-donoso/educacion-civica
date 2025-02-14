adc2e9d921907730ff839bee99ad7e26bcbd12b5
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-14 01:39:19 -0300

    agrego icono

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
@@ -6,7 +5,9 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
+import { BookIcon } from "lucide-react"; // Importa el ícono que desees
 import "./globals.css";
+
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -42,10 +43,11 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Educación Cívica</Link>
-                    <div className="flex items-center gap-2">
-
-                    </div>
+                    <Link href="/" className="flex items-center gap-2">
+                      <BookIcon size={24} className="text-foreground" />
+                      Educación Cívica
+                    </Link>
+                    <div className="flex items-center gap-2"></div>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
