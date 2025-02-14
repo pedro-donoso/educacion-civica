9e77f5f20a68abbdfca7b20667650c61b2ccf912
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-13 21:16:17 -0300

    actualizo mensaje

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,13 @@ export default async function Home() {
       <Hero />
 
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <p className="text-gray-300 mb-4">
-          Estamos encantados de tenerte aquí. A continuación, te mostramos los pasos que puedes seguir para comenzar.
+        <p className="text-gray-500 mb-4">
+          Estamos encantados de tenerte aquí. A continuación, Inicia sesion con las siguientes opciones:
+
+          Iniciar sesion
+
+          Email: probono@mail.com
+          Contraseña: 123456
         </p>
       </main>
     </>
