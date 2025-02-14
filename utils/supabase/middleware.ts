54b1f180c81ff38bfb4d01ce823ca40ee9fb0a3a
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-14 01:03:54 -0300

    elimino comentarios

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -35,8 +35,6 @@ export const updateSession = async (request: NextRequest) => {
       },
     );
 
-    // This will refresh session if expired - required for Server Components
-    // https://supabase.com/docs/guides/auth/server-side/nextjs
     const user = await supabase.auth.getUser();
 
     // protected routes
@@ -50,9 +48,7 @@ export const updateSession = async (request: NextRequest) => {
 
     return response;
   } catch (e) {
-    // If you are here, a Supabase client could not be created!
-    // This is likely because you have not set up environment variables.
-    // Check out http://localhost:3000 for Next Steps.
+
     return NextResponse.next({
       request: {
         headers: request.headers,
