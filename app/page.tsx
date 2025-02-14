a5e5a802a172d52aec81972b84a03faeb94ec881
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-13 21:19:08 -0300

    agrego cuadro con datos

--- app/page.tsx
+++ app/page.tsx
@@ -24,13 +24,23 @@ export default async function Home() {
 
       <main className="flex-1 flex flex-col gap-6 px-4">
         <p className="text-gray-500 mb-4">
-          Estamos encantados de tenerte aquí. A continuación, Inicia sesion con las siguientes opciones:
-
-          Iniciar sesion
-
-          Email: probono@mail.com
-          Contraseña: 123456
+          Estamos encantados de tenerte aquí. A continuación, inicia sesión con
+          las siguientes opciones:
         </p>
+
+        <div className="bg-gray-100 p-4 rounded-md">
+          <h2 className="text-lg font-semibold text-gray-700">
+            Iniciar sesión
+          </h2>
+          <div className="mt-2">
+            <p className="text-gray-600">
+              Email: <span className="font-medium">probono@mail.com</span>
+            </p>
+            <p className="text-gray-600">
+              Contraseña: <span className="font-medium">123456</span>
+            </p>
+          </div>
+        </div>
       </main>
     </>
   );
